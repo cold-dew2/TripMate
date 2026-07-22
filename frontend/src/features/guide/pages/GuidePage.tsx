ed6ee19bd4428dee00a2673522ad422fc6e51bff
hanyiseul <seulli_hys@daum.net>
2026-07-22 16:00:12 +0900

    컴포넌트 가이드 정리

---
+++ frontend/src/features/guide/pages/GuidePage.tsx
@@ -0,0 +1,66 @@
+import Button from '@/shared/components/button/Button'
+import Input from '@/shared/components/Input/Input';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Link, useNavigate } from 'react-router-dom'
+
+const GuidePage = () => {
+
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+
+  const handleSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const trimmed = query.trim();
+    if (!trimmed) return;
+    navigate(`/search?q=${encodeURIComponent(trimmed)}`)
+  }
+  
+  return (
+    <>
+      <div>button</div>
+
+      <div>
+
+        <Button as={Link} to="/" variant="ghost" icon size="icon" text={t("home.notice")} img="/icons/icon-notice.png" />
+        <Button as={Link} to="/mypage" size="icon" icon text={t("home.mypage")} img="/icons/icon-my.png" />
+
+        <Button text="Primary Button" />
+        <Button text="Primary Button" disabled />
+        <Button text="secondary Button" variant="secondary" />
+        <Button text="secondary Button" variant="secondary" disabled />
+        <Button text="ghost Button" variant="ghost" />
+        <Button text="ghost Button" variant="ghost" disabled />
+        <Button text="destructive Button" variant="destructive" />
+        <Button text="destructive Button" variant="destructive" disabled />
+
+        <div className="buttons">
+          <Button text="secondary Button" variant="secondary" />
+          <Button text="Primary Button" />
+        </div>
+
+        <div className="buttons flex-2">
+          <Button text="secondary Button" variant="secondary" />
+          <Button text="Primary Button" />
+        </div>
+      </div>
+
+      <div>input</div>
+      <div className="input">
+
+
+        <form onSubmit={handleSearchSubmit}>
+          <Input className="header-search" label={t("home.searchLabel")} placeholder={t("home.searchPlaceholder")} name="search" id="home-search" blind onChange={(e) => setQuery(e.target.value)} />
+        </form>
+
+        <Input label={t("home.searchLabel")} placeholder={t("home.searchPlaceholder")} name="search" id="home-search" blind />
+        <Input label={t("home.searchLabel")} placeholder={t("home.searchPlaceholder")} name="search" id="home-search" blind disabled />
+        <Input label={t("home.searchLabel")} placeholder={t("home.searchPlaceholder")} name="search" id="home-search" blind readonly />
+      </div>
+    </>
+  
+  )
+}
+
+export default GuidePage
