3afbf043a6ff9852637e53ddcd8382c1d5f63b15
hanyiseul <seulli_hys@daum.net>
2026-07-22 14:27:15 +0900

    input 컴포넌트

--- frontend/src/shared/components/Input/Input.tsx
+++ frontend/src/shared/components/Input/Input.tsx
@@ -1,16 +1,26 @@
+import "./Input.css"
+
 interface InputProps {
-    id?: string;
-    label?: string;
-    type?: string;
-    error?: string;  
+  id?: string;
+  className?: string;
+  label: string;
+  blind?: boolean;
+  type?: string;
+  name: string;
+  placeholder: string;
+  disabled?: boolean;
+  readonly?: boolean;
+  error?: string;  
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({type = "text", id, label, error, ...props}: InputProps) => {
+const Input = ({ 
+  type = "text", id, className, label, blind, name, placeholder, disabled, readonly, error, onChange 
+}: InputProps) => {
   return (
-    <div className="form">
-        <label htmlFor={id}>{label}</label>
-        <input type={type} id={id} {...props}/>
-dddd
+    <div className={`form ${className ? `${className}` : ""}`}>
+      <label htmlFor={id} className={blind ? "blind" : ""}>{label}</label>
+      <input type={type} id={id} placeholder={placeholder} name={name} disabled={disabled} readOnly={readonly} onChange={onChange}/>
         {error && <p className="error-msg">{error}</p>}
     </div>
   )
