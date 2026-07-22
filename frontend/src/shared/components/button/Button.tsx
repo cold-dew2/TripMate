7f480528290b098a8022eb0b327e5c444b02abe7
hanyiseul <seulli_hys@daum.net>
2026-07-22 14:27:03 +0900

    버튼 컴포넌트

---
+++ frontend/src/shared/components/button/Button.tsx
@@ -0,0 +1,46 @@
+import React, { type ElementType } from 'react'
+import "./Button.css"
+
+interface Props {
+  as?: ElementType;
+  id?: string;
+  className?: string;
+  type?: "button" | "submit" | "reset";
+  text: string;
+  blind?: boolean;
+  disabled?: boolean;
+  img?: string;
+  icon?: boolean;
+  size?: "icon" | "md" | "lg";
+  variant?: "primary" | "secondary" | "destructive" | "ghost";
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onFocus?: (e: React.FocusEvent<HTMLButtonElement>) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
+}
+
+const Button = ({ 
+  as: Tag = "button", id, className, type = "button", text, blind, disabled, img, icon, size = "md", variant = "primary", onClick, onFocus, ...rest 
+}: Props) => {
+  const isButton = Tag === "button"
+  const classNames = `btn btn-${variant} btn-${size} ${icon ? "btn-icon" : ""} ${disabled ? "is-disabled" : ""} ${className ?? ""}`
+
+  return (
+    <Tag
+      type={isButton ? type : undefined}
+      id={id}
+      className={classNames}
+      disabled={isButton ? disabled : undefined}
+      onClick={onClick}
+      onFocus={onFocus}
+      {...rest}
+    >
+      {img && (
+        <img src={img} alt="" />
+      )}
+      <span className={blind || icon ? 'blind' : ""}>{text}</span>
+    </Tag>
+  )
+}
+
+export default Button
