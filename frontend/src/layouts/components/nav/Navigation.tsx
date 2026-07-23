6882463837881dcca17b779caa0ac2334001a150
hanyiseul <seulli_hys@daum.net>
2026-07-23 22:30:03 +0900

    네비게이션 작업

--- frontend/src/layouts/components/nav/Navigation.tsx
+++ frontend/src/layouts/components/nav/Navigation.tsx
@@ -1,7 +1,31 @@
+import { useTranslation } from "react-i18next";
+import { NavLink } from "react-router-dom";
+import "./Navigation.css"
+
 const Navigation = () => {
+  const { t } = useTranslation();
+
+  const menus = [
+    {id: 0, path: "/", label: "nav.home", icon: "home"},
+    { id: 1, path: "/places", label: "nav.places", icon: "place"},
+    { id: 2, path: "/groups", label: "nav.groups", icon: "group"},
+    { id: 3, path: "/chat", label: "nav.chat", icon: "chat"},
+    { id: 4, path: "/my", label: "nav.my", icon: "my"},
+  ]
   return (
     <nav>
-      Navigation
+      <ul className="menu-list">
+        {menus.map(menu => (
+          <li key={menu.id}>
+            <NavLink to={menu.path} className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+              <span className="icon">
+                <img src={`/icons/icon-${menu.icon}.png`} alt={`${menu.icon}`} />
+              </span>
+              <span className="title">{t(menu.label)}</span>
+            </NavLink>
+          </li>
+        ))}
+      </ul>  
     </nav>
   )
 }
